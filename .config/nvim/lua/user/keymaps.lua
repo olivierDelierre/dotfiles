73696bb4faafc338b1f4848d031bdabd8bb2a0b8
Olivier Delierre <o.delierre@icloud.com>
2024-06-28 09:14:20 +0200

    [nvim] Removing <w> from split movement shortcuts (e.g.: CTRL+W+H becomes CTRL+H)

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -11,4 +11,9 @@ vim.g.maplocalleader = " "
 
 -- Normal Mode --
 -- Window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
 keymap("n", "<leader>e", ":Lex 30<cr>", opts) -- Leader + E open the explorer
